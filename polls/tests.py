44821914c50df1554e9ea60c03c60b303eefac8b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-11 09:24:04 +0300

    writing tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        pass
+

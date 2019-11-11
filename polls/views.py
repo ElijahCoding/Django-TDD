6453055304203a1c56148e9cb40076a7ac4adf3c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-11 09:19:18 +0300

    refactored to generic views

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 # Create your views here.
 def index(request):

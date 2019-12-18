ad300d7f43831f99d3c7367e421d8a498f394eb0
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-18 15:00:11 +0300

    test_product_detail_unuthenticated

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from django.test import RequestFactory
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
 import pytest
@@ -17,3 +17,15 @@ class TestViews:
 
         response = product_detail(request, pk=1)
         assert response.status_code == 200
+
+    def test_product_detail_unuthenticated(self):
+        mixer.blend('products.Product')
+        path = reverse('detail', kwargs={
+            'pk': 1
+        })
+        request = RequestFactory().get(path)
+        request.user = AnonymousUser()
+
+        response = product_detail(request, pk=1)
+        assert response.status_code == 302
+        assert 'accounts/login' in response.url

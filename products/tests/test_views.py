78a91ac6b095df18a462466ab496ab262dd9dbd5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-18 14:57:44 +0300

    test_product_detail_authenticated

---
+++ products/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.test import RequestFactory
+from django.contrib.auth.models import User
+from products.views import product_detail
+from mixer.backend.django import mixer
+import pytest
+
+@pytest.mark.django_db
+class TestViews:
+    def test_product_detail_authenticated(self):
+        mixer.blend('products.Product')
+        path = reverse('detail', kwargs={
+            'pk': 1
+        })
+        request = RequestFactory().get(path)
+        request.user = mixer.blend(User)
+
+        response = product_detail(request, pk=1)
+        assert response.status_code == 200
